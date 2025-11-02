3b71ffc65f511fa69238e8a07ebc78358453473f
Madhawakavindu <madhawakavindu7@gmail.com>
2025-11-02 09:13:19 +0530

    update

---
+++ lib/auth/login_or_register.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LoginOrRegister extends StatefulWidget {
+  const LoginOrRegister({super.key});
+
+  @override
+  State<LoginOrRegister> createState() => _LoginOrRegisterState();
+}
+
+class _LoginOrRegisterState extends State<LoginOrRegister> {
+  //initially, show login page
+  bool showLoginPage = true;
+
+  //toggle between login and register page
+  void togglePages() {
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
