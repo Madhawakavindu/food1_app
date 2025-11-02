278791c2a35bec80270008b08913bfe2cf90bab2
Madhawakavindu <madhawakavindu7@gmail.com>
2025-11-03 01:11:50 +0530

    update

---
+++ lib/components/my_current_location.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class MyCurrentLocation extends StatelessWidget {
+  const MyCurrentLocation({super.key});
+
+  void openLocationSearchBox(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text("Your location"),
+        content: const TextField(
+          decoration: const InputDecoration(hintText: "Search address.."),
+        ),
+        //cancel button
+        actions: [
+          // cancel
+          MaterialButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text('Cancel'),
+          ),
+          //save
+          MaterialButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text('Save'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Deliver Now",
+            style: TextStyle(color: Theme.of(context).colorScheme.primary),
+          ),
+          GestureDetector(
+            onTap: () => openLocationSearchBox(context),
+            child: Row(
+              children: [
+                //address
+                Text(
+                  "6901 HellyWood Blv",
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                //drop down menu
+                Icon(Icons.keyboard_arrow_down_rounded),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:food_app/components/my_current_location.dart';
 import 'package:food_app/components/my_drawer.dart';
+import 'package:food_app/components/my_sliver_app_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,8 +14,29 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home")),
       drawer: MyDrawer(),
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          MySliverAppBar(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Divider(
+                  indent: 25,
+                  endIndent: 25,
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+                //my current location
+                MyCurrentLocation(),
+
+                //decription box
+              ],
+            ),
+            title: Text("title"),
+          ),
+        ],
+        body: Container(color: Colors.blue),
+      ),
     );
   }
 }
